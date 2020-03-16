69f0c24f5acd16ec0dc04366d02f721a3b25d4ae
Gaurav <gauravgunjal71@gmail.com>
2020-03-16 15:39:42 +0530

    added lowercase, remove punctuation, camelcase operations

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request
+from re import split
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,7 +12,28 @@ def capitalize():
     text = text.upper()
     return render_template('index.html', text=text)
 
+@app.route('/lower', methods=['POST'])
+def lower():
+    text = str(request.form['text'])
+    text = text.lower()
+    return render_template('index.html', text=text)
 
+@app.route('/removepunc', methods=['POST'])
+def removepunc():
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    text = str(request.form['text'])
+    no_punct = ""
+    for char in text:
+        if char not in punctuations:
+            no_punct = no_punct + char
+    return render_template('index.html', text=no_punct)
+
+@app.route('/camelcase', methods=['POST'])
+def camelcase():
+    text = str(request.form['text'])
+    text = ''.join(a.capitalize() for a in split('([^a-zA-Z0-9])', text)
+       if a.isalnum())
+    return render_template('index.html', text=text)
 
 
 app.run(port=4996)
